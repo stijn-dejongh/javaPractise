75b7546aae4a196ff5ba5ce4d21e1ba1474da249
Stijn Dejongh <stijn.c.dejongh@gmail.com>
2018-05-20 00:08:56 +0200

    Added code for three kata's

---
+++ CodeWars/src/DigPow/DigPow.java
@@ -0,0 +1,29 @@
+package DigPow;
+
+import java.util.stream.IntStream;
+
+/**
+ * Created by doji on 19.05.18.
+ *
+ * Code Wars kata: https://www.codewars.com/kata/5552101f47fc5178b1000050/train/java
+ */
+public class DigPow {
+
+
+  public static long digPow(int n, int p) {
+    String digits = Integer.toString(n);
+    double powSum = IntStream.range(0, digits.length())
+        .map(i -> (int) Math.pow((double) (digits.charAt(i) - '0'), (double) p + i)).sum();
+    return (powSum % n) == 0 ? (long) (powSum / n) : -1;
+  }
+
+  // Made this implementation first one because I was scared of rounding errors
+  public static long firstDigPow(int n, int p) {
+    String digits = Integer.toString(n);
+    double powSum = 0;
+    for (int i = 0; i < digits.length(); i++) {
+      powSum += Math.pow((double) (digits.charAt(i) - '0'), (double) p + i);
+    }
+    return (powSum % n) == 0 ? (long) (powSum / n) : -1;
+  }
+}
---
+++ CodeWars/src/Fracts/FractsTest.java
@@ -0,0 +1,15 @@
+package Fracts;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class FractsTest {
+
+  @Test
+  public void testFractions() {
+    long[][] lst;
+    lst = new long[][]{{1, 2}, {1, 3}, {1, 4}};
+    assertEquals("(6,12)(4,12)(3,12)", Fracts.convertFrac(lst));
+  }
+}
